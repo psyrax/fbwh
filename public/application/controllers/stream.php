953527668b213a4326fd4d895da71ee7c5fae99a
yonarox <yonarox@gmail.com>
2012-08-27 13:38:22 -0500

    stream

--- public/application/controllers/stream.php
+++ public/application/controllers/stream.php
@@ -19,18 +19,19 @@ class Stream extends CI_Controller {
 	public function get($type){
 		global $facebook;
 		$access_token = $facebook->getAccessToken();
+		$fbid=$this->session->userdata('id');
 
 		$sql="SELECT post_id, viewer_id, app_id, source_id, created_time, attribution, actor_id, message, app_data, action_links, attachment, comments, likes, privacy, type, permalink, xid
-      	FROM stream WHERE source_id = ".$user_data['id']." limit 100";
+      	FROM stream WHERE source_id = ".$fbid." limit 100";
 
 		$data = $facebook->api(array('method' => 'fql.query','query' => $sql));
 		
-		$fbid=$this->session->userdata('id');
+		$result=null;
 		
 		  foreach($data as $post){
 		      if(key_exists('attachment',$post) && key_exists('media',$post['attachment'])){
 			  foreach($post['attachment']['media'] as $media){
-			      if($media['type']==$media_type)
+			      if($media['type']==$type)
 				  $result[]=$post;
 			  }
 		      }
