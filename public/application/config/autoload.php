78d62f1ff0f4ed6c5c674c426f696b501f558d36
yonarox <yonarox@gmail.com>
2012-08-27 12:56:01 -0500

    autoload helper

--- public/application/config/autoload.php
+++ public/application/config/autoload.php
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('template','session');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url');
+$autoload['helper'] = array('url','facebook');
 
 
 /*
