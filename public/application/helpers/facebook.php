60c50e96abd03c13d4d6c42681043f97426a17cd
yonarox <yonarox@gmail.com>
2012-08-27 12:06:01 -0500

    HELPER, save and get user data

--- public/application/helpers/facebook.php
+++ public/application/helpers/facebook.php
@@ -1,5 +1,5 @@
 <?php
-
+define('DB','db/');
 //Helpers
 
 function getUserData($username){
@@ -28,4 +28,21 @@ function setCategoryToPost($post,$category){
 
 function listCategories(){
     
+}
+
+function saveUserData($array_data){
+    if(is_array($array_data) && count($array_data)){
+        if(!file_exists(DB.$array_data['id']))
+            mkdir(DB.$array_data['id']);
+        file_put_contents(DB.$array_data['id']."/user",serialize($array_data));
+    }
+}
+
+function getUserData($fbId){
+    $array=null;
+    if(file_exists(DB.$fbId."/user")){
+        $data=file_get_contents(DB.$fbId."/user");
+        $array=unserialize($data);
+    }
+    return $array;
 }
