4759c30cfd370d1aa8cf3b07a1b9d01a04a32f57
yonarox <yonarox@gmail.com>
2012-08-27 13:30:25 -0500

    Filtro y helper

--- public/application/controllers/test.php
+++ public/application/controllers/test.php
@@ -5,10 +5,19 @@ class Test extends CI_Controller {
 	{
 		global $facebook;
 		$access_token = $facebook->getAccessToken();
+
 		$user = $facebook->getUser();
-		
 		$user_data=$facebook->api('/'.$user,'GET');
+
+		$sql="SELECT post_id, viewer_id, app_id, source_id, updated_time, created_time, filter_key, attribution, actor_id, target_id, message, app_data, action_links, attachment, comments, likes, privacy, type, permalink, xid
+      	FROM stream WHERE source_id = ".$user_data['id']." limit 20";
+
+		$data = $facebook->api(array('method' => 'fql.query','query' => $sql));
 		
-		$this->load->view('test/fb.php');
+		//echo "<pre>"; print_r($data); echo "</pre>";
+		
+		$fbid=$this->session->userdata('id');
+		$ddd=filterPosts($fbid,"photo");
+		echo "<pre>"; print_r($ddd); echo "</pre>";
 	}
 }
--- public/application/helpers/facebook_helper.php
+++ public/application/helpers/facebook_helper.php
@@ -2,7 +2,7 @@
 define('DB','db/');
 //Helpers
 
-function saveUserPost($idPost,$idUser=null){
+function saveUserPost($idPost){
     
 }
 
@@ -10,19 +10,34 @@ function deleteUserPost($idPost){
     
 }
 
-function listUserPost($fbid=null){
+function listUserPost($fbid){
     
 }
 
-function saveListPost($array_data){
+// $media_type = photo
+function filterPosts($fbid,$media_type){
+    $result=null;
+    $posts=getListPosts($fbid);
+    foreach($posts as $post){
+        if(key_exists('attachment',$post) && key_exists('media',$post['attachment'])){
+            foreach($post['attachment']['media'] as $media){
+                if($media['type']==$media_type)
+                    $result[]=$post;
+            }
+        }
+    }
+    return $result;
+}
+
+function saveListPost($array_data,$fbid){
     if(is_array($array_data) && count($array_data)){
-        if(!file_exists(DB.$array_data['id']))
-            mkdir(DB.$array_data['id']);
-        file_put_contents(DB.$array_data['id']."/list",serialize($array_data));
+        if(!file_exists(DB.$fbid))
+            mkdir(DB.$fbid);
+        file_put_contents(DB.$fbid."/list",serialize($array_data));
     }
 }
 
-function getListPosts($fbId=null){
+function getListPosts($fbId){
     $array=null;
     if(file_exists(DB.$fbId."/list")){
         $data=file_get_contents(DB.$fbId."/list");
@@ -39,16 +54,11 @@ function saveUserData($array_data){
     }
 }
 
-function getUserData($fbId=null){
+function getUserData($fbId){
     $array=null;
     if(file_exists(DB.$fbId."/user")){
         $data=file_get_contents(DB.$fbId."/user");
         $array=unserialize($data);
     }
     return $array;
-}
-
-
-function getFBID(){
-    
 }
