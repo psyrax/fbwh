2ccb9fd6d2435f9cd45b5d2c49fc2a2f2a126978
Psyrax <psyrax@opiumgarden.org>
2012-08-27 11:48:11 -0500

    Login

--- public/application/controllers/welcome.php
+++ public/application/controllers/welcome.php
@@ -1,12 +1,28 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
+	 public function __construct()
+       {
+            parent::__construct();
+       }
 	public function index()
 	{
 		global $facebook;
+		$params = array(
+  		'scope' => 'read_stream, friends_likes',
+  		'redirect_uri' => site_url('welcome/init')
+		);
 		$data=array();
-		$data['login_url']=$facebook->getLoginUrl();
-		print_r($data);
+		$data['login_url']=$facebook->getLoginUrl($params);
 		$this->template->load('template', 'login', $data);
+	}
+	public function init(){
+		global $facebook;
+		$access_token = $facebook->getAccessToken();
+		$user = $facebook->getUser();
+		$user_data=$facebook->api('/'.$user,'GET');
+		print_r($user_data);
+		
+
 	}
 }
